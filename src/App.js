b7d7cf5bf029a30d6384a8669b33445ddc760456
Ivelin Dimitrov <ivelin.dimitrov966@gmail.com>
2018-10-08 23:47:03 +0300

    Connected with Clarifai API and getting response

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Register from './components/Register/Register';
 import Signin from './components/Signin/Signin';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
 //import logo from './logo.svg';
 import './App.css';
@@ -23,7 +24,39 @@ const particlesOptions = {
   }
 }
 
+const app = new Clarifai.App({
+  apiKey: 'efcd035226db47089ad8ab69c87ba826'
+});
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '', 
+
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);  // vzima ot ImageLinkForm i go vadi v konzolata
+  }
+
+
+  // to add image to after the form in FaceRecognition
+  // 
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models.predict('a403429f2ddf4b49b307e318f00e528b', 
+    'https://www.sanger.ac.uk/sites/default/files/gaffney-group.jpg').then(
+      function(response) {
+        console.log(response);
+      },
+      function(err) {
+        console.error(err);
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -31,7 +64,9 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange}  
+          onButtonSubmit={this.onButtonSubmit} />
         <FaceRecognition />
         <Register />
         <Signin />
